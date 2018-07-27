4198849bbca47dc056b5ebaf29529a7e9bf5614b
Dana Abdelhalim <dana.abdelhalim18@gmail.com>
2018-07-27 16:33:46 +0000

    automatically backed up by learn

---
+++ iteration.rb
@@ -0,0 +1,13 @@
+# dog.rb
+people = ["dana", "rayna", "caroline"]
+ 
+people.each do |element|
+  puts "says hi #{element}"
+end 
+
+
+food = ["pizza", "sushi", "taco", "gyros"]
+
+food.each do |type|
+  puts "i love #{type}"
+end 
